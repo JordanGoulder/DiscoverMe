2bafcab8e636c7edc8b67d8ca317c32fdd458f9a
Jordan Goulder <jordan@jordangoulder.com>
2023-08-05 00:28:42 -0400

    Initial commit

---
+++ DiscoverMe.Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// DiscoverMe service port number
+const int discoverMePort = 33455;
+
+// hello request datagram
+byte[] helloRequestDatagram = "hello?"u8.ToArray();
+
+// hello response message
+const string helloResponseMessage = "hello!";
+
+Console.WriteLine($"{DateTimeOffset.Now:s} DiscoverMe client started");
+
+// UdpClient to send/receive datagrams
+UdpClient client = new();
+
+// Something to hold the address of the server
+IPEndPoint? serverEndPoint = null;
+
+// Keep screaming until someone responds
+while (true)
+{
+    // hello? Is there anybody out there...
+    Console.WriteLine($"{DateTimeOffset.Now:s} hello? Is there anybody out there");
+    client.Send(helloRequestDatagram, helloRequestDatagram.Length, new(IPAddress.Broadcast, discoverMePort));
+
+    // Used to stop waiting for a response after a bit
+    CancellationTokenSource cts = new();
+    cts.CancelAfter(1000);
+
+    Console.WriteLine($"{DateTimeOffset.Now:s} Listening to see if someone responds...");
+    try
+    {
+        // Wait for a response
+        var response = await client.ReceiveAsync(cts.Token);
+
+        // I hear something. Did they say 'hello!' ?
+        string message = Encoding.UTF8.GetString(response.Buffer);
+        if (message == helloResponseMessage)
+        {
+            // Let me just jot down their address
+            serverEndPoint = response.RemoteEndPoint;
+
+            // No more screaming necessary
+            break;
+        }
+
+        // Got a response but it isn't what I expected it to be so I should probably ignore it
+        Console.WriteLine(
+            $"{DateTimeOffset.Now:s} Someone at {response.RemoteEndPoint} heard me, but I don't know what '{message}' means.");
+
+        // Wait a bit before we try again
+        await Task.Delay(1000);
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine($"{DateTimeOffset.Now:s} Hmm, no one responded.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{DateTimeOffset.Now:s} Opps, something went wrong: {ex.Message}");
+
+        // Don't know what do with this error so I'll just quit
+        break;
+    }
+
+    Console.WriteLine($"{DateTimeOffset.Now:s} Let's scream again");
+}
+
+// Yay! Someone heard me!!!
+Console.WriteLine(serverEndPoint is not null
+    ? $"{DateTimeOffset.Now:s} Someone at {serverEndPoint} heard me and answered!"
+    : $"{DateTimeOffset.Now:s} I am all alone");
+
+Console.WriteLine($"{DateTimeOffset.Now:s} DiscoverMe client stopped.");
---
+++ DiscoverMe.Server/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// DiscoverMe service port number
+const int discoverMePort = 33455;
+
+// hello response datagram
+byte[] helloResponseDatagram = "hello!"u8.ToArray();
+
+// hello request message
+const string helloRequestMessage = "hello?";
+
+Console.WriteLine($"{DateTimeOffset.Now:s} DiscoverMe service started.");
+
+// UdpClient to send/receive datagrams
+UdpClient server = new();
+
+// Let me listen on the broadcast address even if someone else is already listening on it
+server.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+// Don't prevent anyone else from listening to the broadcast address
+server.ExclusiveAddressUse = false;
+
+// Only listen to broadcast messages
+IPEndPoint broadcastEndPoint = new(IPAddress.Broadcast, discoverMePort);
+server.Client.Bind(broadcastEndPoint);
+
+// Keep listening to see if anyone screams
+while (true)
+{
+    Console.WriteLine($"{DateTimeOffset.Now:s} Listening to see if anyone screams...");
+    try
+    {
+        // Wait for a scream
+        var request = await server.ReceiveAsync();
+
+        // I heard something. Did they say 'hello?' ?
+        string message = Encoding.UTF8.GetString(request.Buffer);
+        if (message == helloRequestMessage)
+        {
+            // They said 'hello?'. Let me just holler back so they know where I am
+
+            Console.WriteLine(
+                $"{DateTimeOffset.Now:s} Someone at {request.RemoteEndPoint} was screaming.");
+
+            Console.WriteLine(
+                $"{DateTimeOffset.Now:s} Responding to {request.RemoteEndPoint} so they know where I am.");
+
+            // Send a response
+            await server.SendAsync(helloResponseDatagram, request.RemoteEndPoint);
+        }
+        else
+        {
+            // Got a request but it isn't what I expected it to be so I should probably ignore it
+            Console.WriteLine(
+                $"{DateTimeOffset.Now:s} Someone at {request.RemoteEndPoint} was screaming, but I don't know what '{message}' means.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{DateTimeOffset.Now:s} Opps, something went wrong: {ex.Message}");
+
+        // Don't know what do with this error so I'll just quit
+        break;
+    }
+}
+
+Console.WriteLine($"{DateTimeOffset.Now:s} DiscoverMe service stopped.");
